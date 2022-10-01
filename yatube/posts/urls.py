aa5ce73bba4e4174a5f7ae992e7ed730936f6f20
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-02 02:59:14 +0300

    Буксую!!!!

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -4,5 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
-    path('group/<slug:slug>', views.group_posts),
+    path(
+        'group/<int:pk>',
+        views.group_posts
+    ),
 ]
