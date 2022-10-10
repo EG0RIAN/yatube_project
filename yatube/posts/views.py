0effaf374b9ac59a67258d53126138052d0aee04
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-11 00:20:49 +0300

    namespace

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,7 +5,16 @@ from django.http import HttpResponse
 
 def index(request):
     template = 'posts/index.html'
-    return render(request, template) 
+    main_text = 'Это главная страница проекта Yatube'
+    context = {
+        'main_text': main_text
+    }
+    return render(request, template, context) 
 
 def group_posts(request, pk):
-    return HttpResponse(f'Хуендок {pk}')
+    template = 'posts/group_list.html'
+    main_text = 'Здесь будет информация о группах проекта Yatube'
+    context = {
+        'main_text': main_text
+    }
+    return render(request, template, context)
