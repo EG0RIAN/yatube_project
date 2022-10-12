66fac2f88433bde84b419f4dfc0ade9babca0f30
Егор Архипцев <e.arkhiptsev@gmail.com>
2022-10-12 16:19:46 +0300

    OperationalError at /admin/posts/post

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -3,9 +3,10 @@ from django.urls import path
 from . import views
 app_name = "posts"
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path(
         'group/<int:pk>',
-        views.group_posts
+        views.group_posts,
+        name='group_list'
     ),
 ]
